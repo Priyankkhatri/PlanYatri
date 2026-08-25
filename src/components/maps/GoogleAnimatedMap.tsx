697526f871ae8b3d762c0904d53acf0fc5322866
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-25 09:41:23 +0530

    feat(maps): create GoogleAnimatedMap component with smooth markers

---
+++ src/components/maps/GoogleAnimatedMap.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export const GoogleAnimatedMap: React.FC = () => {
+  return (
+    <div style={{ width: '100%', height: 300, background: '#18181B', color: '#FFF', borderRadius: 12, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <span>Google Vector Map Animation Layer</span>
+    </div>
+  );
+};
