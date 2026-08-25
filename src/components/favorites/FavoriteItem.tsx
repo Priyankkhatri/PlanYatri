2760f29da3f4ab9a0107926f727d1107eba0d1e3
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-25 09:41:27 +0530

    feat(favorites): create FavoriteItem card component

---
+++ src/components/favorites/FavoriteItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@/styles/Favorites.css';
+
+export const FavoriteItem: React.FC<{ title: string; image: string }> = ({ title, image }) => {
+  return (
+    <div className="fav-item">
+      <img src={image} alt={title} style={{ width: '100%', height: 180, objectFit: 'cover' }} />
+      <div style={{ padding: 14 }}>
+        <h4>{title}</h4>
+      </div>
+    </div>
+  );
+};
