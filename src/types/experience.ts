4871c4e5ae44f9c097e1c27bcae3b9e86a620ff6
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-25 09:40:19 +0530

    types(experiences): add TypeScript definitions for adventure experiences

---
+++ src/types/experience.ts
@@ -0,0 +1,10 @@
+export interface Experience {
+  id: string;
+  title: string;
+  category: string;
+  location: string;
+  duration: string;
+  price: number;
+  image: string;
+  rating: number;
+}
