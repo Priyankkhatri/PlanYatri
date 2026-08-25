1ebc26513fdc28f2ec2db2517c2ba442f114bafa
Priyankkhatri <priyankkhatri1980@gmail.com>
2026-08-25 10:12:31 +0530

    chore(root): clean root directory to leave strictly /frontend, /backend, and README.md

--- setup.ps1
+++
@@ -1,8 +0,0 @@
-Remove-Item package.json -ErrorAction SilentlyContinue
-npx -y create-next-app@latest planyatri --typescript --eslint --tailwind --app --src-dir --import-alias "@/*" --use-npm --no-turbopack
-if (Test-Path planyatri) {
-  Move-Item -Path "planyatri\*" -Destination "." -Force
-  Move-Item -Path "planyatri\.eslint*" -Destination "." -Force
-  Move-Item -Path "planyatri\.gitignore" -Destination "." -Force
-  Remove-Item -Path "planyatri" -Recurse -Force
-}
